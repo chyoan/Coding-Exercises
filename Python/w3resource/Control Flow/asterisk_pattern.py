1e00d6cf4ff108128b62dd3e857c4a6ac3d39711
Hyo <138879492+chyoan@users.noreply.github.com>
2024-03-28 21:04:05 +0800

    Create asterisk_pattern.py

---
+++ Python/w3resource/Control Flow/asterisk_pattern.py
@@ -0,0 +1,23 @@
+#Write a Python program to construct the following pattern, using a nested for loop.
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
+
+n = 5
+
+for i in range(n):
+    for j in range(i+1):
+        print('*', end=' ')
+    print()
+    
+for i in range(n):
+    for j in range(i, n-1):
+        print('*', end=' ')
+    print()
